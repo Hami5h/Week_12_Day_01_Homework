a1eccc25ef7c745f8d413800dc2954f409bd0ecc
Hamish <hamishfpso@gmail.com>
2017-12-11 16:49:17 +0000

    Added cat Array and removed cats from Index.html

--- public/app.js
+++ public/app.js
@@ -1,4 +1,14 @@
+const cats = [
+  {name: 'Boba', favouriteFood: 'Sock Fluff', image: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"},
+  {name: 'Barnaby', favouriteFood: 'Tuna', image: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"},
+  {name: 'Max', favouriteFood: 'Whiskas Temptations', image: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"}
+]
+
+
 const app = function() {
+  for(let cat of cats) {
+    createCat(cat.image, cat.name, cat.favouriteFood);
+  }
 
 
 
